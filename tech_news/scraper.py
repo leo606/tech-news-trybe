bfed3d98c046eef74653143846718fb056f802ef
Leonardo Ferreira <leonardoferreira@protonmail.com>
2022-03-09 22:43:28 -0300

    requisito 1 - fetch url

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,6 +1,19 @@
+import time
+import requests
+
+
 # Requisito 1
 def fetch(url):
-    """Seu código deve vir aqui"""
+    time.sleep(1)
+    try:
+        response = requests.get(url, timeout=3)
+        response.raise_for_status()
+    except requests.ReadTimeout:
+        return
+    except requests.HTTPError:
+        return
+    else:
+        return response.text
 
 
 # Requisito 2
