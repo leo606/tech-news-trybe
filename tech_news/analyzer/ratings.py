340fc2d1a5f8eb4c6e6d1569a072e5b69f41064e
Leonardo Ferreira <leonardoferreira@protonmail.com>
2022-03-11 09:52:08 -0300

    requisito 10 - top 5 news

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,6 +1,25 @@
+from pprint import pprint
+from tech_news.database import db
+
+
 # Requisito 10
 def top_5_news():
-    """Seu código deve vir aqui"""
+    news_list = db.news.aggregate(
+        [
+            {
+                "$addFields": {
+                    "sum": {"$sum": ["$shares_count", "$comments_count"]}
+                }
+            },
+            {"$sort": {"sum": -1}},
+            {"$limit": 5},
+        ]
+    )
+
+    return [(item["title"], item["url"]) for item in news_list]
+
+
+pprint(top_5_news())
 
 
 # Requisito 11
